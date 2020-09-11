cf1b646499e40a0876398e1ee505f01d7d818897
Stacey Louis <stacey-louis@lambdastudents.com>
2020-09-10 21:57:06 -0400

    mvp 3

--- Desktop/lambda-orders/src/main/java/com/lambda/orders/orders/controllers/AgentController.java
+++ Desktop/lambda-orders/src/main/java/com/lambda/orders/orders/controllers/AgentController.java
@@ -17,21 +17,21 @@ public class AgentController {
     @Autowired
     private AgentsServices agentsServices;
 
-    //    GET /agents/agent/{id} - Returns the agent and their customers with the given agent id
+    //
     @GetMapping(value = "/agent/{id}", produces = {"application/json"})
     public ResponseEntity<?> getAgentByID(@PathVariable long id) {
         Agent a = agentsServices.getById(id);
         return new ResponseEntity<>(a, HttpStatus.OK);
     }
 
-    //    DELETE /agents/unassigned/{agentcode} - Deletes an agent if they are not assigned to a customer
+
     @DeleteMapping(value = "/unassigned/{id}")
     public ResponseEntity<?> deleteUnassignedAgents(@PathVariable long id) {
         agentsServices.deleteUnassigned(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    //    GET /agents/unassigned - Returns a list of agents with no customers
+
     @GetMapping(value = "/unassigned", produces = {"application/json"})
     public ResponseEntity<?> getUnassignedAgents() {
         List<Agent> agentList = agentsServices.getUnassigned();
